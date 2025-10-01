8df2ba4a97e09d1eea4d86d739031ad87384ceb5
JP-Grimes <grimesjp@email.sc.edu>
2025-09-30 21:27:01 -0400

    Initial frontend commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/")
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MTG Land Simulator</h1>
+      <p>{data ? data.message : "Loading..."}</p>
     </div>
   );
 }
